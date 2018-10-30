29f66d8ce0d47116d2ed7cbeae9d16bf53d443cc
sspaloniak <s.spaloniak@gmail.com>
2018-10-30 12:52:22 +0100

    Create Models Class

---
+++ MistrzowieWynajmu/Model/Address.cs
@@ -0,0 +1,9 @@
+﻿namespace MistrzowieWynajmu.Model
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ MistrzowieWynajmu/Model/Owner.cs
@@ -0,0 +1,10 @@
+﻿namespace MistrzowieWynajmu.Model
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
