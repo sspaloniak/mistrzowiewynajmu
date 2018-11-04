f97d0ba0239ce7e1a5838353c173de53da233124
sspaloniak <s.spaloniak@gmail.com>
2018-11-04 11:18:53 +0100

    Init First Angular Model Property

---
+++ MistrzowieWynajmu/ClientApp/app/models/Property.ts
@@ -0,0 +1,12 @@
+﻿export class Property {
+    constructor(
+        public propertyId: number,
+        public type: string,
+        public description: string,
+        public rooms: number,
+        public area: number,
+        public washer: boolean,
+        public iron: boolean,
+        public fridge: boolean
+    ) { }
+}
